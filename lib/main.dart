edf9b4386502bb643013afd0d4ab04ab75281b08
mahmoud-bayoumi <mahmoudbayoumi110@gmail.com>
2025-08-23 00:14:12 +0300

    init the app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlanMateApp());
+}
+class PlanMateApp extends StatelessWidget {
+  const PlanMateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
