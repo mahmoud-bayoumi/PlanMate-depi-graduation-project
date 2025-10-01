5cfd388cae3ead2316c8e1dae6a64f85ba8257f5
OmarElnagmy-0 <omar.elnagmy003@gmail.com>
2025-10-01 18:05:49 +0300

    initial commit for login and signup features

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const PlanMateApp());
 }
+
 class PlanMateApp extends StatelessWidget {
   const PlanMateApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Placeholder(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('PlanMate')),
+        body: const Center(child: Text('Welcome to PlanMate!')),
+      ),
     );
   }
 }
