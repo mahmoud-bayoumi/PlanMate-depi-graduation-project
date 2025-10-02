0b352dd87fc5a6f824cd2bece08169ebb9a59381
mahmoud-bayoumi <mahmoudbayoumi110@gmail.com>
2025-10-02 17:09:00 +0300

    create splash view and add logo in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:planmate_app/features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const PlanMateApp());
 }
+
 class PlanMateApp extends StatelessWidget {
   const PlanMateApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Placeholder(),
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
     );
   }
 }
