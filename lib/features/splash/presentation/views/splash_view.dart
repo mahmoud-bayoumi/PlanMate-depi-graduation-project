d5c62a944b1346e17956ab9083a8734d095cd3c4
mahmoud-bayoumi <mahmoudbayoumi110@gmail.com>
2025-10-02 17:21:17 +0300

    create the logo container

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:planmate_app/core/utils/constants.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Color(kPrimaryColor),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
     );
   }
 }
