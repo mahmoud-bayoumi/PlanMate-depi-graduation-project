8e5d20ec3f89029b652c9fa02e2c13f84512279f
menna <mennaheshham9@gmail.com>
2025-10-13 21:18:44 +0300

    added profile screen into main navigation

--- lib/features/navigation_bar/presentation/view/navigate_main_view.dart
+++ lib/features/navigation_bar/presentation/view/navigate_main_view.dart
@@ -3,6 +3,7 @@ import 'package:planmate_app/core/utils/constants.dart';
 import 'package:planmate_app/features/favourites/presentation/view/favourite_view_body.dart';
 import 'package:planmate_app/features/home/presentation/view/home_view_body.dart';
 import 'package:planmate_app/features/home/presentation/view/widgets/icon_with_under_line.dart';
+import 'package:planmate_app/features/profile/presentation/views/profile_view.dart';
 import 'package:planmate_app/your_event_list_screen.dart';
 
 class NavigateMainView extends StatefulWidget {
@@ -27,7 +28,7 @@ class _NavigateMainViewState extends State<NavigateMainView> {
             HomeViewBody(),
             FavouriteViewBody(),
             EventsListScreen(),
-            Text("profile"),
+            ProfileView(),
           ],
         ),
       ),
