4dc296cdfb32348ad9232c28de0b1189900ce295
FayezSabry <georgesabry35@gmail.com>
2025-10-15 17:56:09 +0300

    edit chat states

--- lib/features/group_chat/presentation/bloc/chat_state.dart
+++ lib/features/group_chat/presentation/bloc/chat_state.dart
@@ -1,17 +1,30 @@
-abstract class ChatState {}
+import 'package:equatable/equatable.dart';
+
+abstract class ChatState extends Equatable {
+  const ChatState();
+
+  @override
+  List<Object?> get props => [];
+}
 
 class ChatInitial extends ChatState {}
 
-
 class ChatLoading extends ChatState {}
-
 
 class ChatLoaded extends ChatState {
   final List<Map<String, dynamic>> messages;
-  ChatLoaded(this.messages);
+
+  const ChatLoaded(this.messages);
+
+  @override
+  List<Object?> get props => [messages];
 }
 
 class ChatError extends ChatState {
   final String message;
-  ChatError(this.message);
+
+  const ChatError(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
