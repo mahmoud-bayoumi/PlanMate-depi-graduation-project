fdfa70c6e40db69028a7f8bc94486ee5117d959e
FayezSabry <georgesabry35@gmail.com>
2025-10-15 19:19:52 +0300

    edit group chat view and add BlocProvider to provide chat bloc to view

--- lib/features/group_chat/presentation/views/group_chat_view.dart
+++ lib/features/group_chat/presentation/views/group_chat_view.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/utils/constants.dart';
 import 'widgets/group_chat_body.dart';
+import '../bloc/chat_bloc.dart';
+import '../bloc/chat_event.dart';
 
 class GroupChatView extends StatelessWidget {
   const GroupChatView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: const Text(
-          'Event Group Chat',
-          style: TextStyle(
-            color: Color(kPrimaryColor),
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
+    return BlocProvider(
+      create: (_) => ChatBloc()..add(LoadMessages()),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text(
+            'Event Group Chat',
+            style: TextStyle(
+              color: Color(kPrimaryColor),
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
+        body: const GroupChatBody(),
       ),
-      body: const GroupChatBody(),
     );
   }
 }
