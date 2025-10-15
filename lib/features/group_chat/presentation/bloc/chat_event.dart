b4af060773b12ba98987ae2f922f952353c05b33
FayezSabry <georgesabry35@gmail.com>
2025-10-15 17:51:27 +0300

    edit chat events

--- lib/features/group_chat/presentation/bloc/chat_event.dart
+++ lib/features/group_chat/presentation/bloc/chat_event.dart
@@ -1,14 +1,39 @@
-abstract class ChatEvent {}
+import 'package:equatable/equatable.dart';
 
+abstract class ChatEvent extends Equatable {
+  const ChatEvent();
+
+  @override
+  List<Object?> get props => [];
+}
 
 class LoadMessages extends ChatEvent {}
 
+
 class SendMessage extends ChatEvent {
   final String content;
-  SendMessage(this.content);
+
+  const SendMessage(this.content);
+
+  @override
+  List<Object?> get props => [content];
 }
 
 class MessagesUpdated extends ChatEvent {
   final List<Map<String, dynamic>> messages;
-  MessagesUpdated(this.messages);
+
+  const MessagesUpdated(this.messages);
+
+  @override
+  List<Object?> get props => [messages];
+}
+
+
+class MessagesUpdatedError extends ChatEvent {
+  final String errorMessage;
+
+  const MessagesUpdatedError(this.errorMessage);
+
+  @override
+  List<Object?> get props => [errorMessage];
 }
