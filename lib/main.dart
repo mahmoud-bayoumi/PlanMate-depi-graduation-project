9d5b1e732a66309f999d580cf817b7e8a46e6e58
FayezSabry <georgesabry35@gmail.com>
2025-11-03 21:05:01 +0200

    fix(Chat bloc)update Firebase user display name during registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'features/authentication/bloc/auth_bloc.dart';
 import 'features/authentication/bloc/auth_event.dart';
 import 'features/authentication/services/auth_service.dart';
+import 'features/group_chat/presentation/views/group_chat_view.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 import 'firebase_options.dart';
 
@@ -14,6 +16,14 @@ void main() async {
   // Load environment variables and initialize Firebase
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseAuth.instance.createUserWithEmailAndPassword(
+  //   email: 'fady5@gmail.com',
+  //   password: '123456',
+  // );
+   await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: 'fady4@gmail.com',
+    password: '123456',
+  );
 
   runApp(const PlanMateApp());
 }
@@ -29,7 +39,7 @@ class PlanMateApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'PlanMate',
         theme: ThemeData(fontFamily: 'Poppins'),
-        home: const SplashView(),
+        home: const GroupChatView(),
       ),
     );
   }
